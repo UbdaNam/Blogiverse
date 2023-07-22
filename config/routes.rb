63482292162024592742408491c242cde0737c8d
Abdurahim .M <Abdugreat2@gmail.com>
2023-07-23 00:11:43 +0300

    Fix routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
+  resources :users, only: [:index]
   get '/users/:id/posts/new', to: 'posts#new'
-  get '/users/:id/posts/:post_id', to: 'posts#show'
+  resources :users, only: [:show] do
+    resources :posts, only: [:index, :show]
+  end
   post '/users/:id/posts/create', to: 'posts#create'
-  get '/users/:id/posts/:post_id/comments/new', to: 'comments#new'
-  post '/users/:id/posts/:post_id/comments/create', to: 'comments#create'
-  post '/users/:id/posts/:post_id/likes/create', to: 'likes#create'
+  get 'users/:id/posts/:post_id/comments/new', to: 'comments#new'
+  post 'users/:id/posts/:post_id/comments/create', to: 'comments#create'
+  post 'users/:id/posts/:post_id/likes/create', to: 'likes#create'
 
 end
