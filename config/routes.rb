40382567fa07ae99b4ab94d9d541707f38050b8a
UbdaNam <abdugreat2@gmail.com>
2023-07-22 12:27:37 -0700

    Updated link

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   resources :users, only: [:index]
+  get '/users/:id/posts/new', to: 'posts#new'
   resources :users, only: [:show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show]
   end
+  post '/users/:id/posts/create', to: 'posts#create'
   get 'users/:id/posts/:post_id/comments/new', to: 'comments#new'
   post 'users/:id/posts/:post_id/comments/create', to: 'comments#create'
   post 'users/:id/posts/:post_id/likes/create', to: 'likes#create'
