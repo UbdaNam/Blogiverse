a0cc39273e39736f30086a9e188092cdf89b8151
Abdurahim .M <Abdugreat2@gmail.com>
2023-07-24 17:11:01 +0300

    Fix n+1 problem when fetching posts and their comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @user_id = params[:user_id]
     @user = User.find(@user_id)
-    @posts = User.find(@user_id).posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
