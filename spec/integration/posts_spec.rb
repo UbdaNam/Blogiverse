e96e7220526cd32cb17104ba09d504ead518fadc
Abdurahim .M <Abdugreat2@gmail.com>
2023-07-26 19:32:08 +0300

    Fix linter errors

--- spec/integration/posts_spec.rb
+++ spec/integration/posts_spec.rb
@@ -34,13 +34,15 @@ RSpec.describe 'Posts features', type: :feature do
       expect(current_path).to eq(user_post_path(@first_user, @first_post))
     end
   end
-  describe "post show page" do
+  describe 'post show page' do
     before(:each) do
-      @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-      @second_user = User.create(name: 'Marad', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Student from Vitnam.')
+      @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                bio: 'Teacher from Mexico.')
+      @second_user = User.create(name: 'Marad', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                 bio: 'Student from Vitnam.')
       @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
-      @first_comment = Comment.create(post: @first_post, author: @second_user, text: 'Hi Denver!' )
-      @second_comment = Comment.create(post: @first_post, author: @second_user, text: 'Wow Denver!' )
+      @first_comment = Comment.create(post: @first_post, author: @second_user, text: 'Hi Denver!')
+      @second_comment = Comment.create(post: @first_post, author: @second_user, text: 'Wow Denver!')
     end
     it "Displays all post's data" do
       visit user_post_path(@first_user, @first_post)
