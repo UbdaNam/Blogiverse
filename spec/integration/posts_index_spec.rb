40d7bb3fcf94843f2fdbb3fec3b97f0a058a846c
Abdurahim .M <Abdugreat2@gmail.com>
2023-07-27 06:40:11 +0300

    Fix linter errors

--- spec/integration/posts_index_spec.rb
+++ spec/integration/posts_index_spec.rb
@@ -1,36 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "User's post index page", type: :feature do
+  before(:each) do
+    @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Mexico.')
+    @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+    @second_post = Post.create(author: @first_user, title: 'Hello2', text: 'This is my second post')
+    @third_post = Post.create(author: @first_user, title: 'Hello3', text: 'This is my third post')
+    @fourth_post = Post.create(author: @first_user, title: 'Hello4', text: 'This is my fourth post')
+    @fifth_post = Post.create(author: @first_user, title: 'Hello5', text: 'This is my fifth post')
+    @first_comment = Comment.create(post: @first_post, author: @first_user, text: 'Hi Lala!')
+  end
 
-    before(:each) do
-      @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                                bio: 'Teacher from Mexico.')
-      @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
-      @second_post = Post.create(author: @first_user, title: 'Hello2', text: 'This is my second post')
-      @third_post = Post.create(author: @first_user, title: 'Hello3', text: 'This is my third post')
-      @fourth_post = Post.create(author: @first_user, title: 'Hello4', text: 'This is my fourth post')
-      @fifth_post = Post.create(author: @first_user, title: 'Hello5', text: 'This is my fifth post')
-      @first_comment = Comment.create(post: @first_post, author: @first_user, text: 'Hi Lala!')
-    end
+  it "displays all user's data" do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content(@first_user.name)
+    expect(page).to have_css("img[src='#{@first_user.photo}']")
+    expect(page).to have_content(@first_user.posts_counter)
+    expect(page).to have_content(@first_post.title)
+    expect(page).to have_content(@first_post.text)
+    expect(page).to have_content(@second_post.text)
+    expect(page).to have_content(@third_post.text)
+    expect(page).to have_content(@first_comment.text)
+    expect(page).to have_content(@first_post.comments_counter)
+    expect(page).to have_content(@first_post.likes_counter)
+    expect(page).to have_selector('.pagination')
+  end
 
-    it "displays all user's data" do
-      visit user_posts_path(@first_user)
-      expect(page).to have_content(@first_user.name)
-      expect(page).to have_css("img[src='#{@first_user.photo}']")
-      expect(page).to have_content(@first_user.posts_counter)
-      expect(page).to have_content(@first_post.title)
-      expect(page).to have_content(@first_post.text)
-      expect(page).to have_content(@second_post.text)
-      expect(page).to have_content(@third_post.text)
-      expect(page).to have_content(@first_comment.text)
-      expect(page).to have_content(@first_post.comments_counter)
-      expect(page).to have_content(@first_post.likes_counter)
-      expect(page).to have_selector('.pagination')
-    end
-
-    it "clicking on a user redirects to that user's show page" do
-      visit user_posts_path(@first_user)
-      click_link @first_post.text
-      expect(current_path).to eq(user_post_path(@first_user, @first_post))
-    end
+  it "clicking on a user redirects to that user's show page" do
+    visit user_posts_path(@first_user)
+    click_link @first_post.text
+    expect(current_path).to eq(user_post_path(@first_user, @first_post))
+  end
 end
--- spec/integration/users_show_spec.rb
+++ spec/integration/users_show_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Single user show page', type: :feature do
-
   before(:each) do
-      @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                                bio: 'Teacher from Mexico.')
-      @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
-      @second_post = Post.create(author: @first_user, title: 'Hello2', text: 'This is my second post')
-      @third_post = Post.create(author: @first_user, title: 'Hello3', text: 'This is my third post')
-      @fourth_post = Post.create(author: @first_user, title: 'Hello4', text: 'This is my fourth post')
-      @fifth_post = Post.create(author: @first_user, title: 'Hello5', text: 'This is my fifth post')
-    end
+    @first_user = User.create(name: 'LaLa', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Mexico.')
+    @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+    @second_post = Post.create(author: @first_user, title: 'Hello2', text: 'This is my second post')
+    @third_post = Post.create(author: @first_user, title: 'Hello3', text: 'This is my third post')
+    @fourth_post = Post.create(author: @first_user, title: 'Hello4', text: 'This is my fourth post')
+    @fifth_post = Post.create(author: @first_user, title: 'Hello5', text: 'This is my fifth post')
+  end
 
-    it "displays all user's data" do
-      visit user_path(@first_user)
-      expect(page).to have_content(@first_user.name)
-      expect(page).to have_css("img[src='#{@first_user.photo}']")
-      expect(page).to have_content(@first_user.posts_counter)
-      expect(page).to have_content(@first_user.bio)
-      expect(page).to have_content(@third_post.text)
-      expect(page).to have_content(@fourth_post.text)
-      expect(page).to have_content(@fifth_post.text)
-      expect(find_button('See all posts')).to be_truthy
-    end
+  it "displays all user's data" do
+    visit user_path(@first_user)
+    expect(page).to have_content(@first_user.name)
+    expect(page).to have_css("img[src='#{@first_user.photo}']")
+    expect(page).to have_content(@first_user.posts_counter)
+    expect(page).to have_content(@first_user.bio)
+    expect(page).to have_content(@third_post.text)
+    expect(page).to have_content(@fourth_post.text)
+    expect(page).to have_content(@fifth_post.text)
+    expect(find_button('See all posts')).to be_truthy
+  end
 
-    it "clicking on a user redirects to that user's show page" do
-      visit user_path(@first_user)
-      click_link @third_post.text
-      expect(current_path).to eq(user_post_path(@first_user, @third_post))
-    end
+  it "clicking on a user redirects to that user's show page" do
+    visit user_path(@first_user)
+    click_link @third_post.text
+    expect(current_path).to eq(user_post_path(@first_user, @third_post))
+  end
 
-    it "clicking on see all posts button will redirect to user's posts index page" do
-      visit user_path(@first_user)
-      click_link('See all posts')
-      expect(current_path).to eq(user_posts_path(@first_user))
-    end
+  it "clicking on see all posts button will redirect to user's posts index page" do
+    visit user_path(@first_user)
+    click_link('See all posts')
+    expect(current_path).to eq(user_posts_path(@first_user))
+  end
 end
